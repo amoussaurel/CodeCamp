e608e8f34a527508d9e1576f272070d0ad28ae10
amoussaurel <amoussaurel@gmail.com>
2017-10-20 10:11:44 +0200

    4th day Coding

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-/*$(document).ready(
-  function()
-  {
-    $("h1").toggle(4000);
-  }
-);*/
-$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/musics?api_key=keycBA59JDS0jdm1J",
+$(document).ready(function() {
+  //   $("h1").toggle(4000);
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall?api_key=keygXbTJs6OHQIT6C",
 					function(data) {
+
           	console.log(data);
             $.each(data.records, function(key,val){
               	console.log(val.fields.title);
@@ -17,3 +15,20 @@ $.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/musics?api_key=keycBA59
 
 					}
 );
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall%20OldSchool?api_key=keygXbTJs6OHQIT6C",
+					function(data) {
+
+          	console.log(data);
+            $.each(data.records, function(key,val){
+              	console.log(val.fields.title);
+
+                $('<li>'+val.fields.title+'</li>').appendTo('#oldmusiques');
+
+                $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#oldmusiques');
+            });
+
+					}
+);
+
+});
