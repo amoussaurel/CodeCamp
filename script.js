e1cac771c13a1994cc8a03974ec3f5b771b4d168
amoussaurel <amoussaurel@gmail.com>
2017-10-20 15:26:24 +0200

    5th Day Coding

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-  //   $("h1").toggle(4000);
+  	$("h1").hide();
+		$("h1").fadeIn(5000);
 
-$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall?api_key=keygXbTJs6OHQIT6C",
+
+/*$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall?api_key=keygXbTJs6OHQIT6C",
 					function(data) {
 
           	console.log(data);
@@ -14,21 +16,81 @@ $.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall?api_
             });
 
 					}
-);
+);*/
 
 $.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall%20OldSchool?api_key=keygXbTJs6OHQIT6C",
+					function(data) {
+					console.log(data);
+					$.each(data.records, function(key,val){
+					console.log(val.fields.likes);
+
+					$('<li>'+val.fields.likes+'<input type = "button" value ="Vote +1" id="bouton">'+'</li>').appendTo('#oldmusiques');
+
+					$("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#oldmusiques');
+					});
+
+
+					}
+);
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/Scott%20Songs?api_key=keygXbTJs6OHQIT6C",
 					function(data) {
 
           	console.log(data);
             $.each(data.records, function(key,val){
               	console.log(val.fields.title);
 
-                $('<li>'+val.fields.title+'</li>').appendTo('#oldmusiques');
+                $('<li>'+val.fields.title+'</li>').appendTo('#scottmusiques');
 
-                $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#oldmusiques');
+                $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#scottmusiques');
             });
 
 					}
+);
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/Lopez?api_key=keygXbTJs6OHQIT6C",
+					function(data) {
+
+          	console.log(data);
+            $.each(data.records, function(key,val){
+              	console.log(val.fields.title);
+
+                $('<li>'+val.fields.title+'</li>').appendTo('#lopezmusiques');
+
+                $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#lopezmusiques');
+            });
+
+					}
+);
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/Alpha?api_key=keygXbTJs6OHQIT6C",
+					function(data) {
+
+          	console.log(data);
+            $.each(data.records, function(key,val){
+              	console.log(val.fields.title);
+
+                $('<li>'+val.fields.title+'</li>').appendTo('#alphamusiques');
+
+                $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#alphamusiques');
+            });
+
+					}
+);
+
+$.getJSON("https://api.airtable.com/v0/appmmDV7S4fYyGaJe/TopList%20Musicall?api_key=keygXbTJs6OHQIT6C",
+                    function(data) {
+              console.log(data);
+            $.each(data.records, function(key,val){
+                  console.log(val.fields.likes);
+
+               $('<li>'+val.fields.likes+'<input type = "button" value ="Vote +1" id="bouton">'+'</li>').appendTo('#musiques');
+
+               $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+            });
+
+
+                    }
 );
 
 });
